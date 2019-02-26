e4e4f3ed6e48c940d90adc7476f8c3500ba1cedf
Fabien Dehopré <fabien@dehopre.com>
2019-02-26 18:35:45 +0100

    create the dbcontext

---
+++ backend/Netatmo.Dashboard.Data/NetatmoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Netatmo.Dashboard.Core.Models;
+
+namespace Netatmo.Dashboard.Data
+{
+    public class NetatmoDbContext : DbContext
+    {
+        public NetatmoDbContext(DbContextOptions options)
+            : base(options)
+        {
+            // these are mutually exclusive, migrations cannot be used with EnsureCreated()
+            // Database.EnsureCreated();
+            Database.Migrate();
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Station> Stations { get; set; }
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<DashboardData> DashboardData { get; set; }
+    }
+}
