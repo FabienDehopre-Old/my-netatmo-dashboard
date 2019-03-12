e26c5a972b0d515806360b14c699a204540a5c08
Fabien Dehopré <fabien@dehopre.com>
2019-03-12 21:07:50 +0100

    add missing file for sentry error handling

---
+++ frontend/src/app/sentry-error-handler.ts
@@ -0,0 +1,10 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import * as Sentry from "@sentry/browser";
+
+@Injectable()
+export class SentryErrorHandler extends ErrorHandler {
+  handleError(error: any): void {
+    Sentry.captureException(error.originalError || error);
+    super.handleError(error);
+  }
+}
