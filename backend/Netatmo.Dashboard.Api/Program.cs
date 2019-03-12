ea82b935cd7f6ff76c9d8ff8cd63ed2fd54e600e
Fabien Dehopré <fabien@dehopre.com>
2019-03-12 14:12:13 +0100

    install and configure sentry for the backend

--- backend/Netatmo.Dashboard.Api/Program.cs
+++ backend/Netatmo.Dashboard.Api/Program.cs
@@ -12,6 +12,7 @@ namespace Netatmo.Dashboard.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseSentry();
     }
 }
