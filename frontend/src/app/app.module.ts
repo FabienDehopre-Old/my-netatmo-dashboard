a87a66f859c29c52ef3d30cdec47304f481ed31f
Fabien Dehopré <fabien@dehopre.com>
2019-03-12 14:25:26 +0100

    install and configure sentry for the frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,14 @@
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import * as Sentry from "@sentry/browser";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SentryErrorHandler } from './sentry-error-handler';
+
+Sentry.init({
+  dsn: "https://2dac4e9cc7814299add137cbfd63b940@sentry.io/1413628"
+});
 
 @NgModule({
   declarations: [
@@ -12,7 +18,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
