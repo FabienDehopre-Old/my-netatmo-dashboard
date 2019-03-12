568fd57cafbc7b913b693e1e5ca589ddc5fb310e
Fabien Dehopré <fabien@dehopre.com>
2019-03-12 21:54:41 +0100

    WIP: adding auth implicit flow to app

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,24 +1,83 @@
-import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import * as Sentry from "@sentry/browser";
+import {
+  AuthModule, AuthWellKnownEndpoints,
+  OidcConfigService,
+  OidcSecurityService,
+  OpenIDImplicitFlowConfiguration,
+} from 'angular-auth-oidc-client';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SentryErrorHandler } from './sentry-error-handler';
+import { HomeComponent } from './pages/home/home.component';
 
 Sentry.init({
   dsn: "https://2dac4e9cc7814299add137cbfd63b940@sentry.io/1413628"
 });
 
+export function loadConfig(oidcConfigService: OidcConfigService): () => void {
+  console.log('APP_INITIALIZER STARTING');
+  return () => oidcConfigService.load(`${window.location.origin}/assets/oidc-implicit-flow-config.json`);
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule.forRoot()
   ],
-  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
+  providers: [
+    { provide: ErrorHandler, useClass: SentryErrorHandler },
+    OidcConfigService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: loadConfig,
+      deps: [OidcConfigService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    private readonly oidcSecurityService: OidcSecurityService,
+    private readonly oidcConfigService: OidcConfigService
+  ) {
+    this.oidcConfigService.onConfigurationLoaded.subscribe(() => {
+      const oidcImplicitFlowConfig = new OpenIDImplicitFlowConfiguration();
+      oidcImplicitFlowConfig.stsServer = this.oidcConfigService.clientConfiguration.stsServer;
+      oidcImplicitFlowConfig.redirect_url = this.oidcConfigService.clientConfiguration.redirect_url;
+      oidcImplicitFlowConfig.client_id = this.oidcConfigService.clientConfiguration.client_id;
+      oidcImplicitFlowConfig.response_type = this.oidcConfigService.clientConfiguration.response_type;
+      oidcImplicitFlowConfig.scope = this.oidcConfigService.clientConfiguration.scope;
+      oidcImplicitFlowConfig.post_logout_redirect_uri = this.oidcConfigService.clientConfiguration.post_logout_redirect_uri;
+      oidcImplicitFlowConfig.start_checksession = this.oidcConfigService.clientConfiguration.start_checksession;
+      oidcImplicitFlowConfig.silent_renew = this.oidcConfigService.clientConfiguration.silent_renew;
+      oidcImplicitFlowConfig.silent_renew_url = this.oidcConfigService.clientConfiguration.silent_renew_url;
+      oidcImplicitFlowConfig.post_login_route = this.oidcConfigService.clientConfiguration.post_login_route;
+      oidcImplicitFlowConfig.forbidden_route = this.oidcConfigService.clientConfiguration.forbidden_route;
+      oidcImplicitFlowConfig.unauthorized_route = this.oidcConfigService.clientConfiguration.unauthorized_route;
+      oidcImplicitFlowConfig.log_console_debug_active = this.oidcConfigService.clientConfiguration.log_console_debug_active;
+      oidcImplicitFlowConfig.log_console_warning_active = this.oidcConfigService.clientConfiguration.log_console_warning_active;
+      oidcImplicitFlowConfig.max_id_token_iat_offset_allowed_in_seconds = this.oidcConfigService.clientConfiguration.max_id_token_iat_offset_allowed_in_seconds;
+
+      const authWellKnownEndpoints = new AuthWellKnownEndpoints();
+      authWellKnownEndpoints.setWellKnownEndpoints(this.oidcConfigService.wellKnownEndpoints);
+
+      this.oidcSecurityService.setupModule(
+        oidcImplicitFlowConfig,
+        authWellKnownEndpoints
+      );
+    });
+
+    console.log('APP STARTING');
+  }
+}
---
+++ frontend/src/app/authorization.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  CanLoad,
+  Router
+} from '@angular/router';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard implements CanActivate, CanLoad {
+  constructor(private readonly router: Router, private readonly oidcSecurityService: OidcSecurityService) {}
+
+  canActivate(): Observable<boolean> {
+    return this.checkUser();
+  }
+
+  canLoad(): Observable<boolean> {
+    return this.checkUser();
+  }
+
+  private checkUser(): Observable<boolean> {
+    return this.oidcSecurityService.getIsAuthorized()
+      .pipe(
+        tap(isAuthorized => {
+          console.log(`AuthorizationGuard: isAuthorized = ${isAuthorized}`);
+          if (!isAuthorized) {
+            this.router.navigate(['/unauthorized']);
+          }
+        })
+      )
+  }
+}
