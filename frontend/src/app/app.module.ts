b9711045e57bcc7dcd8c908a2a6efc4f10a70649
Fabien Dehopré <fabien@dehopre.com>
2019-03-14 14:51:26 +0100

    add an http interceptor to send the access token to api

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as Sentry from '@sentry/browser';
@@ -8,13 +8,14 @@ import { VERSION } from '../version';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { MaterialModule } from './material.module';
 import { CallbackComponent } from './pages/callback/callback.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { SentryErrorHandler } from './services/sentry-error-handler';
+import { AuthInterceptor } from './services/auth.interceptor';
 import { ConfigService } from './services/config.service';
-import { LayoutComponent } from './components/layout/layout.component';
+import { SentryErrorHandler } from './services/sentry-error-handler';
 
 Sentry.init({
   dsn: 'https://2dac4e9cc7814299add137cbfd63b940@sentry.io/1413628',
@@ -32,7 +33,8 @@ export function initApp(configService: ConfigService): () => void {
   imports: [BrowserAnimationsModule, AppRoutingModule, HttpClientModule, MaterialModule],
   providers: [
     { provide: ErrorHandler, useClass: SentryErrorHandler },
-    { provide: APP_INITIALIZER, useFactory: initApp, deps: [ConfigService], multi: true }
+    { provide: APP_INITIALIZER, useFactory: initApp, deps: [ConfigService], multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent],
 })
---
+++ frontend/src/app/services/auth.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private readonly authService: AuthService) { }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!this.authService.isAuthenticated()) {
+      return next.handle(req);
+    }
+
+    const accessToken = localStorage.getItem('ACCESS_TOKEN');
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${accessToken}`)
+    });
+    return next.handle(authReq);
+  }
+}
